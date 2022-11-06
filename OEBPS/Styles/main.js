477f7cfe258f2b17a322db52dfe0959b7b0f0c0a
douglarek <douglarek@gmail.com>
2022-11-06 23:42:14 +0800

    一些修复

--- OEBPS/Styles/main.js
+++ OEBPS/Styles/main.js
@@ -11,10 +11,36 @@ function changeColor(e) {
   return true;
 }
 
+var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+        var p=a[i].split('=', 2);
+        if (p.length == 1)
+            b[p[0]] = "";
+        else
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+})(window.location.search.substr(1).split('&'));
+
 window.onload = function() {
   var href = window.location.href.split('#');
   if (typeof href !== 'undefined' && href.length > 1) {
     g = document.getElementById(href[1]);
     g.style.backgroundColor = '#dafbe1';
+  }
+
+  var z = qs['zhu'];
+  if (z === '0') {
+    var a = document.getElementsByClassName("jiazhu");
+    for (var i = 0; i < a.length; i++) {
+      a.item(i).style.display = 'none';
+    }
+    var a = document.getElementsByClassName("jiaozhusup");
+    for (var i = 0; i < a.length; i++) {
+      a.item(i).style.display = 'none';
+    }
   }
 };
